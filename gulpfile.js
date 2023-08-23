6819bbb6ad5ae74e03831c35910c3266b47a4c15
victor <victor.freire.oliveira@gmail.com>
2023-08-22 22:14:09 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const webp = require('gulp-webp');
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
+
+function webpImage() {
+    return src('./src/images/*')
+        .pipe(webp())
+        .pipe(dest('./dist/images'))
+}
+
+exports.default = gulp.parallel(styles, scripts, webpImage);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*js', gulp.parallel(scripts))
+}
