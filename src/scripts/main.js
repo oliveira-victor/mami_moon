0f3a289f403ad4c9f6e54a0211c5eeab2255a459
victor <victor.freire.oliveira@gmail.com>
2023-08-24 13:58:16 -0300

    navbar transition effect working fine

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,15 @@
 // Navbar scroll effect
 const navbar = document.querySelector('.header');
+const navbarLogo = document.getElementById('logo-txt');
 
 window.addEventListener('scroll', function() {
     const currentPosition = window.scrollY;
 
-    if (currentPosition < 10) {
-        navbar.classList.remove('.change-navbar');
-        navbar.classList.add('.reset-navbar');
+    if (currentPosition < 10) {;
+        navbar.classList.remove('change-navbar');
+        navbarLogo.style.display = 'none'
     } else {
-        navbar.classList.remove('.reset-navbar');
-        navbar.classList.add('.change-navbar');
+        navbar.classList.add('change-navbar');
+        navbarLogo.style.display = 'block'
     }
 })
