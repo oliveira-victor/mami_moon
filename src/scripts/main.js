be73d6b230e0a9da4edc599184873f6c71d9ec55
victor <victor.freire.oliveira@gmail.com>
2023-08-24 21:55:10 -0300

    upgrade: mobile navbar effect

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,18 @@
 // Navbar scroll effect
 const navbar = document.querySelector('.header');
 const navbarLogo = document.getElementById('logo-txt');
-const mobileNav = document.querySelector('mobile-nav');
+const mobileNav = document.querySelector('.mobile-nav');
 
 window.addEventListener('scroll', function() {
     const currentPosition = window.scrollY;
 
     if (currentPosition < 10) {;
         navbar.classList.remove('change-navbar');
-        navbarLogo.style.display = 'none'
+        navbarLogo.style.display = 'none';
+        mobileNav.classList.remove('change-mobile-navbar');
     } else {
         navbar.classList.add('change-navbar');
-        navbarLogo.style.display = 'block'
+        navbarLogo.style.display = 'block';
+        mobileNav.classList.add('change-mobile-navbar');
     }
 })
