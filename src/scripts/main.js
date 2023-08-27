b3b26cd2b8ed3a6621f3f2710c23d66c368e05c8
victor <victor.freire.oliveira@gmail.com>
2023-08-27 01:15:54 -0300

    about section script and pic animation ok

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,3 +16,59 @@ window.addEventListener('scroll', function() {
         mobileNav.classList.add('change-mobile-navbar');
     }
 })
+
+// About tabs
+const tabBio = document.getElementById('tab-bio');
+const tabFunfacts = document.getElementById('tab-funfacts');
+const tabExpect = document.getElementById('tab-expect');
+
+const txtBio = document.querySelector('.txt-bio');
+const txtFunfacts = document.querySelector('.txt-funfacts');
+const txtExpect = document.querySelector('.txt-expect');
+
+tabBio.addEventListener('click', function() {
+    resetTabs();
+    this.classList.add('is-selected');
+    txtBio.style.display = 'block';
+})
+
+tabFunfacts.addEventListener('click', function() {
+    resetTabs();
+    this.classList.add('is-selected');
+    txtFunfacts.style.display = 'block';
+})
+
+tabExpect.addEventListener('click', function() {
+    resetTabs();
+    this.classList.add('is-selected');
+    txtExpect.style.display = 'block';
+})
+
+const resetTabs = () => {
+    tabBio.classList.remove('is-selected');
+    tabFunfacts.classList.remove('is-selected');
+    tabExpect.classList.remove('is-selected');
+
+    txtBio.style.display = 'none';
+    txtFunfacts.style.display = 'none';
+    txtExpect.style.display = 'none';
+}
+
+// Animate about picture
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("rotate");
+        } else {
+        reveals[i].classList.remove("rotate");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
