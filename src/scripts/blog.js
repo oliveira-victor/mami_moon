647cc4b7d0aaef25cfd022dd27b6858a29f8b0a0
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-09-28 17:50:43 -0300

    feat: dynamic blog feed, phone menu

--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -1,15 +1,40 @@
 const loader = document.getElementById('loader-container');
 const postsBox = document.getElementById('posts-box');
+const postsMenu = document.getElementById('posts-menu');
+const phoneMenu = document.getElementById('blog-phone-menu');
+
+function shortenTitle(post) {
+    if (post.length > 30) {
+        return post.slice(0, 30) + "..."
+    }
+    
+    return post
+}
 
 function posts(data) {
     loader.classList.add('disapear')
     for (let i = 0; i < data.length; i++) {
         postsBox.insertAdjacentHTML("beforeend", `
             <div class="post-container fade-in">
-                <h2>${data[i].title.rendered}</h2>
+                <h2 id="${data[i].slug}">${data[i].title.rendered}</h2>
                 <p>${data[i].content.rendered}</p>
             </div>
             <br><hr>
+        `)
+        postsMenu.insertAdjacentHTML("beforeend", `
+            <a href="#${data[i].slug}">
+                <button class="post-btn" type="button">${shortenTitle(data[i].title.rendered)}</button>
+            </a>
+        `)
+        phoneMenu.insertAdjacentHTML("beforeend", `
+            <li>
+                <a href="#${data[i].slug}">
+                    ${shortenTitle(data[i].title.rendered)}
+                </a>
+            </li>
+            <li>
+                ★
+            </li>
         `)
     }
 }
